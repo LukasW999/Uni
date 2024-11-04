ea04d2b0ba9efc57be93bf5494056d1e3c32f7bc
LukasW <lukaswenzel0909@gmail.com>
2024-11-04 23:23:13 +0100

    fakeblackjack und dreieck

--- src/main/java/org/example/homework/H03.java
+++ src/main/java/org/example/homework/H03.java
@@ -2,10 +2,12 @@ package org.example.homework;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
+import java.util.Scanner;
 
 public class H03 {
     public static void main(String[] args) {
-        skonto();
+        dreieck(10);
     }
 
     private static void skonto() {
@@ -29,10 +31,44 @@ public class H03 {
         }
     }
 
-    public static void FakeBlackJack() {
+    public static void fakeBlackJack() {
         int countPlayer = 0;
-        int countAij = 0;
-
-
+        int countAi = 0;
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int userInput = -1;
+            while (userInput < 0 || userInput > 16) {
+                System.out.println("Bitte eine Zahl 1 - 16. 0 zum abbrechen");
+                userInput = sc.nextInt();
+            }
+            countPlayer += userInput;
+            countAi += random.nextInt(1, 17);
+            System.out.println("Spieler Punktzahl: " + countPlayer);
+            System.out.println("Computer Punktzahl: " + countAi);
+            if (userInput == 0 || countPlayer > 50 || countAi > 50) {
+                break;
+            }
+        }
+        if (countAi > 50) {
+            System.out.println("Gewonnen");
+        } else {
+            System.out.println("Verloren");
+        }
     }
+    public static void dreieck(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n-1; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
 }
